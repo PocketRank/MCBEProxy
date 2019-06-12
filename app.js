ffce9292d804701f4426b384c8edc5ec56bae102
nnnlog <sorisem4106@naver.com>
2019-06-13 02:46:55 +0900

    support AppVeyor, domain to minejs.me

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 
 const http = require('http');
 const https = require('https');
-const httpProxy = require('http-proxy');
-
+const proxy = require('http-proxy');
 http.createServer((req, res) => {
     res.writeHead(301, {Location: 'https://' + req.headers.host + req.url});
     res.end();
@@ -11,7 +10,7 @@ http.createServer((req, res) => {
     console.log('listening on port 80');
 });
 
-var proxy = httpProxy.createProxyServer({
+var httpsProxy = proxy.createProxyServer({
     ssl: {
         key: fs.readFileSync(__dirname + '/ssl/private.key'),
         cert: fs.readFileSync(__dirname + '/ssl/cert.pem'),
@@ -19,33 +18,43 @@ var proxy = httpProxy.createProxyServer({
     },
     secure: false
 });
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
+httpsProxy.on('proxyReq', function (proxyReq, req, res, options) {
     proxyReq.setHeader('X-Forwarded-For', (req.headers["X-Forwarded-For"] ||
         req.headers["x-forwarded-for"] ||
         '').split(',')[0] ||
         req.client.remoteAddress);
 });
+
 https.createServer({
     key: fs.readFileSync(__dirname + '/ssl/private.key'),
     cert: fs.readFileSync(__dirname + '/ssl/cert.pem'),
     ca: fs.readFileSync(__dirname + '/ssl/ca.pem')
-}, (req ,res) => {
+}, (req, res) => {
     //pmmp.me: 5000
     //mcbe.cf: 5001
+    //minejs.me: 8051 (http is 8050)
 
     //console.log(req.headers.host);
-    let split = req.headers.host.split('.').reverse();
-    switch (split[1] + '.' + split[0]) {
-        case "mcbe.cf":
-            proxy.web(req, res, {target: 'https://127.0.0.1:5001'});
-            break;
-        case "pmmp.me":
-            proxy.web(req, res, {target: 'https://127.0.0.1:5000'});
-            break;
-        default:
-            res.end("Cannot GET + " + req.headers.host + req.url);
-            return;
+    try {
+        let split = req.headers.host.split('.').reverse();
+        switch (split[1] + '.' + split[0]) {
+            case "mcbe.cf":
+                httpsProxy.web(req, res, {target: 'https://127.0.0.1:5001'});
+                break;
+            case "pmmp.me":
+                httpsProxy.web(req, res, {target: 'https://127.0.0.1:5000'});
+                break;
+            case "minejs.me":
+                httpsProxy.web(req, res, {target: 'https://127.0.0.1:8051'});
+                break;
+            default:
+                res.end("Cannot GET + " + req.headers.host + req.url);
+                return;
+        }
+    } catch (e) {
+        res.end(e);
     }
+
 }).listen(443, () => {
     console.log("listening on port 443");
 });
